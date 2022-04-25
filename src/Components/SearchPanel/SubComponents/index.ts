1f93098556a9b71ac7c863942fb283640fe16005
Victor Gomez <victor.gomez@frubana.com>
2022-04-25 19:41:46 -0300

    NEW PROJECT

--- src/Components/SearchPanel/SubComponents/index.ts
+++
@@ -1,6 +0,0 @@
-import { Box } from './Box';
-import { MainSearcher } from './MainSearcher';
-import { SearchPanelStyled } from './SearchPanelStyled';
-import { Results } from './Results';
-
-export { Box, MainSearcher, Results, SearchPanelStyled };
--- src/Redux/Store.tsx
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import reducer from './Reducers';
-
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
-
-export const store = createStore(reducer, composeEnhancers);
